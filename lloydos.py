b712856c50ae073d49f50a973667c9d847c327f0
tech189 <dlloyd189@gmail.com>
2018-03-26 16:15:09 +0100

    Progress on number variables

--- lloydos.py
+++ lloydos.py
@@ -3,6 +3,7 @@ import re
 with open("test.ld", mode="r") as file:
     line_number = 0
     strings = {}
+    numbers = {}
     for line in file:
         line_number += 1
         if line[0] == "#":
@@ -16,6 +17,13 @@ with open("test.ld", mode="r") as file:
                 #print("String statement equates \"" + string_name + "\" as \"" + string_var + "\"")
                 #save string_name and string_var to a dictionary
                 strings[string_name] = string_var
+            elif line[1:19] == " ἀριθμος ὀνομαζων ":
+                #print("String statement on line " + str(line_number))
+                number_name = re.search("«(.*)» ", line[17:]).group(1)
+                number_var = re.search("ι «(.*)»;", line[17:]).group(1)
+                #print("String statement equates \"" + string_name + "\" as \"" + string_var + "\"")
+                #save string_name and string_var to a dictionary
+                numbers[number_name] = number_var
             else:
                 print("Syntax error on line " + str(line_number))
         elif line[0:5] == "εἰπε ":
@@ -24,6 +32,10 @@ with open("test.ld", mode="r") as file:
             if line[5:26] == "τον λογον ὀνομαζοντα ":
                 string_name = re.search("«(.*)»;", line[26:]).group(1)
                 print(strings[string_name])
+            elif line[5:26] == "τον ἀριθμον ὀνομαζοντα ":
+                #how do you print numbers???????????????????????????????????????/?????????????????
+                number_name = re.search("«(.*)»;", line[26:]).group(1)
+                print(numbers[number_name])
         elif line[0:5] == "λεγε ":
             #print("Infinite print statement on line " + str(line_number))
             #retrieve from dictionary the string and print it!
