a5a74c3340f051928695fa6d2ce57b0a254db8f3
tech189 <dlloyd189@gmail.com>
2018-03-26 18:51:52 +0100

    Arabic numbers now supported, started working on greek numerals

--- lloydos.py
+++ lloydos.py
@@ -1,4 +1,40 @@
-import re
+import re, math
+
+#can't go higher than 999
+numerals = {
+    "0": "οὐδεν",
+    "1": "α",
+    "2": "β",
+    "3": "γ",
+    "π": math.pi,
+    "4": "δ",
+    "5": "ε",
+    "6": "ϝ",
+    "7": "ζ",
+    "8": "η",
+    "9": "θ",
+    "g": "9.81",
+    "10": "ι",
+    "20": "κ",
+    "30": "λ",
+    "40": "μ",
+    "50": "ν",
+    "60": "ξ",
+    "70": "ο",
+    "80": "π",
+    "90": "ϙ",
+    "100": "ρ",
+    "200": "σ",
+    "300": "τ",
+    "400": "υ",
+    "500": "φ",
+    "600": "χ",
+    "700": "ψ",
+    "800": "ω",
+    "900": "Ͳ",
+    #"Ϡ": marker for 1000 so maybe Ϡα = 1000?
+    "c": 299792458
+}
 
 with open("test.ld", mode="r") as file:
     line_number = 0
@@ -6,33 +42,37 @@ with open("test.ld", mode="r") as file:
     numbers = {}
     for line in file:
         line_number += 1
-        if line[0] == "#":
-            print("Comment on line " + str(line_number))
+        #print("DEBUG: Line " + line_number + " is --" + line + "--")
+        if line == "\n":
+            print("DEBUG: Empty line on line " + str(line_number))
+        elif line[0] == "#":
+            print("DEBUG: Comment on line " + str(line_number))
         elif line[0] == "ὁ":
-            #print("Variable statement on line " + str(line_number)) #assuming we won't have variables in other genders
+            #print("DEBUG: Variable statement on line " + str(line_number)) #assuming we won't have variables in other genders
             if line[1:17] == " λογος ὀνομαζων ":
-                #print("String statement on line " + str(line_number))
+                #print("DEBUG: String statement on line " + str(line_number))
                 string_name = re.search("«(.*)» ", line[17:]).group(1)
                 string_var = re.search("ι «(.*)»;", line[17:]).group(1)
-                #print("String statement equates \"" + string_name + "\" as \"" + string_var + "\"")
+                #print("DEBUG: String statement equates \"" + string_name + "\" as \"" + string_var + "\"")
                 #save string_name and string_var to a dictionary
                 strings[string_name] = string_var
             elif line[1:19] == " ἀριθμος ὀνομαζων ":
-                #print("String statement on line " + str(line_number))
+                #print("DEBUG: String statement on line " + str(line_number))
                 number_name = re.search("«(.*)» ", line[17:]).group(1)
                 number_var = re.search("ι «(.*)»;", line[17:]).group(1)
-                #print("String statement equates \"" + string_name + "\" as \"" + string_var + "\"")
+               #print("DEBUG: Number statement equates \"" + number_name + "\" to the value " + number_var + "")
                 #save string_name and string_var to a dictionary
                 numbers[number_name] = number_var
             else:
                 print("Syntax error on line " + str(line_number))
         elif line[0:5] == "εἰπε ":
-            #print("Print statement on line " + str(line_number))
+            #print("DEBUG: Print statement on line " + str(line_number))
             #retrieve from dictionary the string and print it!
+            #print("-"+line[5:28]+"-")
             if line[5:26] == "τον λογον ὀνομαζοντα ":
                 string_name = re.search("«(.*)»;", line[26:]).group(1)
                 print(strings[string_name])
-            elif line[5:26] == "τον ἀριθμον ὀνομαζοντα ":
+            elif line[5:28] == "τον ἀριθμον ὀνομαζοντα ":
                 #how do you print numbers???????????????????????????????????????/?????????????????
                 number_name = re.search("«(.*)»;", line[26:]).group(1)
                 print(numbers[number_name])
@@ -44,4 +84,4 @@ with open("test.ld", mode="r") as file:
                 while True:
                     print(strings[string_name])
         else:
-            print("Sytax error on line " + str(line_number))
+            print("DEBUG: Sytax error on line " + str(line_number))
